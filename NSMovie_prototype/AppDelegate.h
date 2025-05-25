323b8ff85489f1e259f3387ed39d72b084579df3
Gregory John Casamento <greg.casamento@gmail.com>
2025-05-24 20:01:49 -0400

    Initial Commit

---
+++ NSMovie_prototype/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSMovie_prototype
+//
+//  Created by Gregory Casamento on 5/24/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSMovie_prototype/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSMovie_prototype
+//
+//  Created by Gregory Casamento on 5/24/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
