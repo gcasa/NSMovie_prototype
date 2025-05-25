8bc123744e48727883a513c4015e492d5c3ace39
Gregory John Casamento <greg.casamento@gmail.com>
2025-05-25 11:00:52 -0400

    Add NSThread

--- NSMovie_prototype/FFmpegVideoView.m
+++ NSMovie_prototype/FFmpegVideoView.m
@@ -64,7 +64,7 @@
 - (void)startPlayback {
 #ifdef GNUSTEP
     running = YES;
-    decodeThread = [[NSThread alloc] initWithTarget:self selector:@selector(startDecoding) object:nil];
+    decodeThread = [[NSThread alloc] initWithTarget:self selector:@selector(startPlayback) object:nil];
     [decodeThread start];
 #endif
 }
