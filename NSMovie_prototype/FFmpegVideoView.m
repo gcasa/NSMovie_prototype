e5fcbd3833bed7ec8f2b405e2fcc55e9bd7e6a87
Gregory John Casamento <greg.casamento@gmail.com>
2025-05-25 13:31:58 -0400

    Add timer to play the video properly

--- NSMovie_prototype/FFmpegVideoView.m
+++ NSMovie_prototype/FFmpegVideoView.m
@@ -63,14 +63,14 @@
 
 - (void)startPlayback {
 #ifdef GNUSTEP
-    running = YES;
-    // decodeThread = [[NSThread alloc] initWithTarget:self selector:@selector(decodeAndDisplayNextFrame) object:nil];
-    decodeThread = [NSThread scheduledTimerWithTimeInterval: (1.0/30.0)
-						     target: self
-						   selector: @selector(decodeAndDisplayNextFrame)
-						   userInfo: nil
-						    repeats: YES];
-    [decodeThread start];
+  running = YES;
+  // decodeThread = [[NSThread alloc] initWithTarget:self selector:@selector(decodeAndDisplayNextFrame) object:nil];
+  decodeThread = [NSTimer scheduledTimerWithTimeInterval: (1.0/30.0)
+						  target: self
+						selector: @selector(decodeAndDisplayNextFrame)
+						userInfo: nil
+						 repeats: YES];
+  [decodeThread start];
 #endif
 }
 
